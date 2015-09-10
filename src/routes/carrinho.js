cc84cd14328836de243abdbbfa2913a4a000aa39
Thiago Paes <mrprompt@gmail.com>
2015-09-09 21:18:47 -0300

    enviando e-mail após as compras

--- src/routes/carrinho.js
+++ src/routes/carrinho.js
@@ -2,6 +2,7 @@
 
 var router          = require('express').Router();
 var paginate        = require('express-paginate');
+var sendgrid        = require('sendgrid')('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');
 var CarrinhoModel   = require(__dirname + '/../models/carrinho');
 
 router.get('/:usuario', function (req, res) {
@@ -94,7 +95,7 @@ router.post('/:usuario', function (req, res) {
         });
     }
 
-    carrinho.save(function (err, data) {
+    carrinho.save(function (err, resultSave) {
         if (err) {
             return res.status(500).json({
                 object      : 'error',
@@ -105,10 +106,39 @@ router.post('/:usuario', function (req, res) {
             });
         }
 
+        var site = require(__dirname + '/../models/site')
+            .findOne({_id: req.headers.site})
+            .then(function(_site) {
+                var email    = new sendgrid.Email({
+                    to      : _site.emails[0],
+                    from    : 'system@publiciti.com.br',
+                    subject : ' ',
+                    html    : ' '
+                });
+
+                email.setFilters({
+                    'templates': {
+                        'settings': {
+                            'enable': 1,
+                            'template_id' : 'd5e54ad0-718f-4a53-8827-b1dbaeb68238',
+                        }
+                    }
+                });
+
+                email.addSubstitution('%carrinho%', resultSave._id);
+                email.addSubstitution('%status%', resultSave.status);
+                email.addSubstitution('%items%', JSON.stringify(resultSave.items));
+
+                sendgrid.send(email, function(err, json) {
+                    if (err) { return console.error(err); }
+                    return json;
+                });
+            });
+
         res.status(201).json({
             object      : 'object',
             has_more    : false,
-            data        : data,
+            data        : resultSave,
             itemCount   : 1,
             pageCount   : 1
         });
@@ -160,6 +190,35 @@ router.put('/:id/:usuario', function (req, res) {
                         pageCount   : 1
                     });
                 }
+
+                var site = require(__dirname + '/../models/site')
+                    .findOne({_id: req.headers.site})
+                    .then(function(_site) {
+                        var email    = new sendgrid.Email({
+                            to      : _site.emails[0],
+                            from    : 'system@publiciti.com.br',
+                            subject : ' ',
+                            html    : ' '
+                        });
+
+                        email.setFilters({
+                            'templates': {
+                                'settings': {
+                                    'enable': 1,
+                                    'template_id' : 'd5e54ad0-718f-4a53-8827-b1dbaeb68238',
+                                }
+                            }
+                        });
+
+                        email.addSubstitution('%carrinho%', resultSave._id);
+                        email.addSubstitution('%status%', resultSave.status);
+                        email.addSubstitution('%items%', JSON.stringify(resultSave.items));
+
+                        sendgrid.send(email, function(err, json) {
+                            if (err) { return console.error(err); }
+                            return json;
+                        });
+                    });
 
                 res.status(204).json({
                     object      : 'object',
