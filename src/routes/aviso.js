bf4b4c11696ae2c585c0da096e841f4269fcfd16
Thiago Paes <mrprompt@gmail.com>
2015-09-22 01:41:22 -0300

    fixed

--- src/routes/aviso.js
+++ src/routes/aviso.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var router     = require('express').Router();
-var controller = require(__dirnmae + '/../controllers/aviso');
+var controller = require(__dirname + '/../controllers/aviso');
 
 router.get('/', controller.lista(req, res));
 router.get('/:id', controller.abre(req, res));
