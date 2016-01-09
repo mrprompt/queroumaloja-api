0cf281593aac690d8dc06cd7455fd67042a4aa7f
Thiago Paes <mrprompt@gmail.com>
2016-01-09 20:54:56 -0200

    db name

--- src/modules/connection.js
+++ src/modules/connection.js
@@ -5,7 +5,7 @@ var connection_string =
     process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
     process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
     process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-    process.env.OPENSHIFT_APP_NAME;
+    process.env.OPENSHIFT_DATABASE_NAME;
 
 var mongoose = require('mongoose');
     mongoose.connect(connection_string);
