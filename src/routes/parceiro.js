041f58aa8c513817db39a9cd230640723df35d82
Thiago Paes <mrprompt@gmail.com>
2016-01-18 16:55:14 -0200

    upload direto pro cloudinary

--- src/routes/parceiro.js
+++ src/routes/parceiro.js
@@ -10,10 +10,12 @@
 var router      = require('express').Router();
 var connection  = require(__dirname + '/../modules/connection');
 var controller  = require(__dirname + '/../controllers/parceiro');
+var multer      = require('multer');
+var upload      = require(__dirname + '/../modules/upload');
 
 router.get('/', controller.lista);
 router.get('/:id', controller.abre);
-router.post('/', controller.adiciona);
+router.post('/', multer({dest: '/tmp/'}).single('imagem'), upload, controller.adiciona);
 router.put('/:id', controller.atualiza);
 router.delete('/:id', controller.apaga);
 
