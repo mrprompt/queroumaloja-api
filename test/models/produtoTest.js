12238d645826d5e09e43a14995123e75a3837288
Thiago Paes <mrprompt@gmail.com>
2016-03-24 18:14:46 -0300

    mais testes

--- test/models/produtoTest.js
+++ test/models/produtoTest.js
@@ -27,15 +27,39 @@ describe('Produto Model', function () {
 
     it('iniciando sem item e comprador deve retornar os atributos informados como parâmetros', function (done) {
         var dados = {
-            cadastro: new Date(),
-            site    : new ObjectId()
+            titulo      : 'Quam diu etiam furor iste tuus nos eludet?',
+            descricao   : 'Paullum deliquit, ponderibus modulisque suis ratio utitur.',
+            imagem      : {},
+            site        : new ObjectId(),
+            codigo      : 'Petierunt',
+            valor       : 0.01,
+            categoria   : {
+                titulo      : 'Paullum deliquit, ponderibus modulisque suis ratio utitur.',
+                uri         : 'http://localhost/',
+                categoria   : {
+                    titulo  : 'Tu quoque, Brute, fili mi, nihil timor populi, nihil!',
+                    uri     : 'Quis aute iure reprehenderit in voluptate velit esse.'
+                }
+            },
+            cadastro: Date.now(),
+            album: []
         };
 
         var produto = new Produto(dados);
-            produto.should.have.property('cadastro', dados.cadastro);
-            produto.should.have.property('site', dados.site);
 
         should(produto.isNew).is.exactly(true);
+        should(produto).have.property('id');
+        should(produto.isNew).is.exactly(true);
+        should(produto.codigo).is.exactly(dados.codigo);
+        should(produto.titulo).is.exactly(dados.titulo);
+        should(produto.descricao).is.exactly(dados.descricao);
+        should(produto.valor).is.exactly(dados.valor);
+        should(produto.ativo).is.exactly(true);
+        should(produto.categoria).is.exactly(dados.categoria);
+        should(produto.imagem).is.exactly(dados.imagem);
+        should(produto.album).is.a.Array();
+        should(produto.cadastro).be.instanceOf(Date);
+        should(produto.site).is.exactly(dados.site);
 
         done();
     });
