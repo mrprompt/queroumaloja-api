86dd32edef2803e557265eab49d39609bd9a2e80
Thiago Paes <mrprompt@gmail.com>
2016-09-05 03:51:20 -0300

    apidoc

--- routers/login.js
+++ routers/login.js
@@ -3,6 +3,23 @@
 var router = require('express').Router(),
   login = require('../controllers/login');
 
+  /**
+   * @api {post} /login Autentica na API
+   * @apiName LoginAdiciona
+   * @apiGroup Login
+   *
+   * @apiParam {String} email Email de acesso.
+   * @apiParam {String} password Senha de acesso.
+   *
+   * @apiSuccessExample {json} Success-Response:
+   *     HTTP/1.1 201 OK
+   *     {
+   *        token: "f0000-s9992-2sdfsf"
+   *        usuario: {
+   *            ...
+   *          }
+   *    }
+   */
 router.post('/', login.adiciona);
 
 module.exports = router;
