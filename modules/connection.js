b5c5b29a8d974e39fa8a9ded84c7e4d0ab50f5f3
Thiago Paes <mrprompt@gmail.com>
2016-09-14 12:55:44 -0300

    using MONGODB_URI

--- modules/connection.js
+++ modules/connection.js
@@ -7,6 +7,10 @@ var dbPort = process.env.DB_PORT      || '27017';
 var dbName = process.env.DB_NAME      || 'admin';
 var dbUri  = dbUser + ":" + dbPass + "@" + dbHost + ':' + dbPort + '/' + dbName;
 
+if (process.ENV.MONGODB_URI) {
+    dbUri = process.ENV.MONGODB_URI
+}
+
 var mongoose = require('mongoose');
     mongoose.connect(dbUri);
     mongoose.Promise = global.Promise;
