4f9757aec6b77714ef08a2a0c89679960e0c746a
Thiago Paes <mrprompt@gmail.com>
2016-09-15 03:02:59 -0300

    mockando mongoose

--- test/modules/connectionTest.js
+++ test/modules/connectionTest.js
@@ -1,6 +1,26 @@
 'use strict';
 
+var mockery = require('mockery');
+
 describe('Connection Module', function () {
+    before(function() {
+        mockery.enable({
+            warnOnUnregistered: false,
+            warnOnReplace: false
+        });
+
+        mockery.registerMock('mongoose', {
+            Promise: {},
+            connect: function(uri) {
+                return new Object;
+            }
+        });
+    });
+
+    after(function() {
+        mockery.disable()
+    });
+
     it('deve retornar uma objeto mongoose quando process.env.MONGODB_URI estiver definido', function (done) {
         process.env.MONGODB_URI = 'mongodb://foo:bar@127.0.0.1:21017/test';
 
